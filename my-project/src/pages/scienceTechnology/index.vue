e8ac039ec40db476d2a3a07eb5f0d4f82f95e589
陈旭 <chenxu@chenxudeAir.lan>
2018-08-12 23:52:59 +0800

    feat: add common thing request

--- my-project/src/pages/scienceTechnology/index.vue
+++ my-project/src/pages/scienceTechnology/index.vue
@@ -11,6 +11,14 @@
       <img class="img" mode="widthFix" src="https://images.weserv.nl/?url=stavebox.oss-cn-beijing.aliyuncs.com/SomeLabel/C763C9B46AC4420F9F2107724A879221.png" alt="">
     </div>
     <div class="tabs">
+      <div v-for="(tab, index) in tabs" :key="index">{{tab.label}}</div>
+    </div>
+    <div class="view">
+      <div class="common-good" v-for="good in currentGoods" :key="good._id">
+        <div class="good-img-container">
+          <img class="good-img" :src="good.src" alt="" mode="widthFix">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,15 +28,30 @@ export default {
   data () {
     return {
       tabs: [{
-        label: '工具'
+        label: '工具',
+        type: 11
       }, {
-        label: '照明'
+        label: '照明',
+        type: 11
       }, {
-        label: '生存'
+        label: '生存',
+        type: 11
       }, {
         label: '更多'
-      }]
+      }],
+      technologyType: '',
+      currentGoods: []
     }
+  },
+  onLoad (options) {
+    console.info(options, 'mish options')
+    this.technologyType = options.type
+  },
+  async mounted () {
+    const version = 'DST'
+    const result = await this.$http.get(`${this.inline}/${this.technologyType}?version=${version}`)
+    this.currentGoods = result.data.filter(item => item.type === 11)
+    console.info(this.currentGoods, 'mish rerereredsfsf')
   }
 }
 </script>
@@ -37,6 +60,7 @@ export default {
 .science-technology {
   .top-imgs {
     position: relative;
+    height: 160px;
     .bg-img {
       width: 100vw;
       height: 160px;
@@ -51,7 +75,22 @@ export default {
   }
   .tabs {
     height: 30px;
+    display: flex;
+    flex-flow: nowrap row;
+    font-size: 14px;
     background-color: #263238;
+    color: white;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .view {
+    .common-good {
+      .good-img-container {
+        .good-img {
+          width: 48px;
+        }
+      }
+    }
   }
 }
 </style>
